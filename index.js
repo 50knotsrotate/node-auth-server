a67edd59a8b903de0211af55ead059cf6daffd19
= <=>
2019-10-15 08:09:54 -0400

    connect to postgres

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
+
+const { CONNECTION_STRING, PORT } = process.env;
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: CONNECTION_STRING,
+});
+
+client.connect(err => { 
+    if (err) {
+        console.log('Connection error')
+    } else { 
+        console.log('Connected to db')
+    }
+})
+
 const express = require('express');
 
 const users = [];
@@ -9,12 +26,11 @@ app.use(express.json());
 const bcrypt = require('bcrypt');
 const axios = require('axios');
 
-const { CONNECTION_STRING, PORT } = process.env;
 app.post('/auth/signup', (request, response) => {
   const { username, password } = request.body;
 
-/* Make sure the user does not exist */
-    
+  /* Make sure the user does not exist */
+
 
   response.status(200).json({
     username,
@@ -31,6 +47,6 @@ app.post('/auth/signup', (request, response) => {
 //   });
 // });
 
-app.listen(3000, () => {
-  console.log("ayyy");
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
